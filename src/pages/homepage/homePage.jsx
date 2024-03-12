36e5a222c58986d51f8fefaca72b8ab17d58a6f0
Madhur Pathak <madhurpathak000@gmail.com>
2024-03-12 15:53:14 +0530

    commit

--- src/pages/homepage/homePage.jsx
+++ src/pages/homepage/homePage.jsx
@@ -1,22 +1,44 @@
-import './homePage.scss'
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
 
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">
+            Find Real Estate & Get <br /> Your Dream Place..
+          </h1>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In neque
+            rem dolore numquam explicabo incidunt aliquid consequuntur similique
+            atque temporibus iste, necessitatibus, maxime quaerat sunt nobis
+            quisquam. Provident, autem qui.
+          </p>
+          <SearchBar />
 
-const HomePage =  () => {
-    return (
-        <div className="homePage">
-            <div className="textContainer">
-                <div className="wrapper">
-                    <h1 className='title'>Find Real Estate & Get Your Dream Place Find Real Estate & Get Your Dream Place.</h1>
-                    <br />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In neque rem dolore numquam explicabo incidunt aliquid consequuntur similique atque temporibus iste, necessitatibus, maxime quaerat sunt nobis quisquam. Provident, autem qui.</p>
-                  
-                </div>
+          <div className="boxes">
+            <div className="box">
+              <h1>10+</h1>
+              <h2>Years Of Experience..</h2>
             </div>
-            <div className="imgContainer">
-                <img src="./bg.png" alt="" />
+            <div className="box">
+              <h1>13+</h1>
+              <h2>Award Gained..</h2>
             </div>
+            <div className="box">
+              <h1>850+</h1>
+              <h2>Property Ready..</h2>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="imgContainer">
+        <img src="./fbgn.png" alt="" />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
